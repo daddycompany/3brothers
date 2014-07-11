2300b0c34bde5071551292464cf3f8f1c6ba98dd
kimsunchul <jssosweet@gmail.com>
2014-07-11 10:12:18 +0900

    tile Effect

--- sun/JS_file/jquery_tile/assets/jquerytail.js
+++ sun/JS_file/jquery_tile/assets/jquerytail.js
@@ -1,83 +1,57 @@
 /**
  * Created by kimsunchul on 2014. 7. 10..
  */
-imageTileClass={};
-imageTileClass.i=0;
-imageTileClass.j=0;
-imageTileClass.bigboxWidth="";
-imageTileClass.bigboxHeight="";
-imageTileClass.imgClass=new Array;
-imageTileClass.intvalue=0;
-imageTileClass.lengthValue=0;
-imageTileClass.limitValue=0;
-imageTileClass.parentValue=0
+sun_imageTileClass={};
+sun_imageTileClass.i=0;
+sun_imageTileClass.j=0;
+sun_imageTileClass.bigboxWidth=0;
+sun_imageTileClass.bigboxHeight=0;
+sun_imageTileClass.imgClass=new Array;
+sun_imageTileClass.intvalue=0;
+sun_imageTileClass.lengthValue=0;
+sun_imageTileClass.limitValue=0;
+sun_imageTileClass.parseIntValue=0
 
-/*
-imageTileClass.imageTile1=function (){
-
-    $.post("http://shining.daddycompany.com/shiningapi/getmylist", {userid: "fe7270@naver.com"}, function (data) {
-        for (imageTileClass.i = 0; imageTileClass.i < 10; imageTileClass.i++) {
-            imageTileClass.imgClass.push(data.list[imageTileClass.i].movie_thumnail);
-        }
-        if(imageTileClass.i==imageTileClass.imgClass.length){
-          // imageTileClass.imgArray();
-        }
-    })
-}
-*/
-
-
-imageTileClass.imageTile=function (eventData) {
-    $("body").append(" <input type='text' id='widthName' />").append((" <input type='text' id='heightName' />")).
-        append((" <input type='text' id='urlName' />")).append("<div id='bigBox'></div>")
-    if(typeof eventData!="undefined"){
-        imageTileClass.imgClass=eventData;
-        if(imageTileClass.imgClass.length>0){
-            imageTileClass.imgArray()
+sun_imageTileClass.imageTile=function (targetData,targetId) {
+    $(".target").attr("id",targetId)
+    if(typeof targetData!="undefined"){
+        sun_imageTileClass.imgClass=targetData;
+        if(sun_imageTileClass.imgClass.length>0){
+            sun_imageTileClass.imgArray(targetId)
         }
     }else{
-        alert("값을 넣으시요")
+        console.log("no image")
     }
 };
 
-
-
-imageTileClass.imgArray=function (){
-    imageTileClass.lengthValue = imageTileClass.imgClass.length
-    imageTileClass.intvalue = Math.sqrt(imageTileClass.imgClass.length)
-    imageTileClass.parentValue=parseInt(imageTileClass.intvalue);
-    if (imageTileClass.lengthValue < 4) {
-        alert("이미지 4개 이상 선택하세요")
-    } else if (imageTileClass.lengthValue >= 4) {
-        if (typeof imageTileClass.intvalue == "int") {
-            imageTileClass.limitValue = imageTileClass.lengthValue - 1;
+sun_imageTileClass.imgArray=function (targetId){
+    sun_imageTileClass.lengthValue = sun_imageTileClass.imgClass.length
+    sun_imageTileClass.intvalue = Math.sqrt(sun_imageTileClass.imgClass.length)
+    sun_imageTileClass.parseIntValue=parseInt(sun_imageTileClass.intvalue);
+    if (sun_imageTileClass.lengthValue < 4) {
+        console.log("please image count 4")
+    } else if (sun_imageTileClass.lengthValue >= 4) {
+        if (typeof sun_imageTileClass.intvalue == "int") {
+            sun_imageTileClass.limitValue = sun_imageTileClass.lengthValue - 1;
         } else {
-            imageTileClass.limitValue = Math.pow(parseInt(imageTileClass.intvalue), 2)
+            sun_imageTileClass.limitValue = Math.pow(parseInt(sun_imageTileClass.intvalue), 2)
         }
-        for (imageTileClass.j = 0; imageTileClass.j<imageTileClass.limitValue; imageTileClass.j++) {
-            $("#bigBox").append("<div id='smallBox" + imageTileClass.j + "' class='smallboxClass'></div>")
-            $("#smallBox" + imageTileClass.j).append("<div class='smallboxImg' style=background-image:url('" + imageTileClass.imgClass[imageTileClass.j] + "')></div>")
-            console.log("ok")
+        for (sun_imageTileClass.j = 0; sun_imageTileClass.j<sun_imageTileClass.limitValue; sun_imageTileClass.j++) {
+            $("#"+targetId).append("<div id='smallBox" + sun_imageTileClass.j + "' class='smallboxClass'></div>")
+            $("#smallBox" + sun_imageTileClass.j).append("<div class='smallboxImg' style=background-image:url('" + sun_imageTileClass.imgClass[sun_imageTileClass.j] + "')></div>")
         }
-        if (imageTileClass.j == imageTileClass.limitValue) {
-            imageTileClass.changeFn();
+        if (sun_imageTileClass.j == sun_imageTileClass.limitValue) {
+            console.log("load complete")
         }
     }
-}
-
-
-imageTileClass.changeFn=function() {
-    $("#widthName,#heightName").change(function () {
-        imageTileClass.bigboxWidth = $("#widthName").val();
-        imageTileClass.bigboxHeight = $("#heightName").val();
-        $("#bigBox").css({"width": imageTileClass.bigboxWidth, "height": imageTileClass.bigboxHeight, "border": "1px solid red"})
-        var imgWidth = (imageTileClass.bigboxWidth/imageTileClass.parentValue).toFixed(3);
-        var imgHeight = (imageTileClass.bigboxHeight/imageTileClass.parentValue).toFixed(3);
-        $(".smallboxClass").css({"width": imgWidth,"height":imgHeight,"float":"left"})
-        console.log(imageTileClass.parentValue)
-    });
-}
-
+    sun_imageTileClass.bigboxWidth = $("#"+targetId).width()
+    sun_imageTileClass.bigboxHeight = $("#"+targetId).height()
+    console.log(sun_imageTileClass.bigboxWidth )
+    $("#"+targetId).css({"width": sun_imageTileClass.bigboxWidth, "height": sun_imageTileClass.bigboxHeight})
+    var imgWidth = (sun_imageTileClass.bigboxWidth/sun_imageTileClass.parseIntValue).toFixed(3);
+    var imgHeight = (sun_imageTileClass.bigboxHeight/sun_imageTileClass.parseIntValue).toFixed(3);
+    $(".smallboxClass").css({"width": imgWidth,"height":imgHeight,"float":"left"})
+};
 
 
 
